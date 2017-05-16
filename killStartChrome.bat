6df97a8ed03263ff5bdcd3154859c79c5d8a3a6a
Carlos Velasco <carlos@2activedesign.com>
2017-05-16 22:23:46 +0200

    update bat

--- killStartChrome.bat
+++ killStartChrome.bat
@@ -1,5 +1,5 @@
 @echo off
 
-taskkill /IM /F chrome.exe /T
+taskkill /IM chrome.exe /T /F
 
 start chrome "https://xtreemze.github.io/KullabergInfoDesk/embedEn.html" -kiosk -allow-running-insecure-content -allow-file-access-from-files -allow-file-access -allow-cross-origin-auth-prompt

1c5863e7fa633d493dce3fc7e4bde2bb498c6fd7
Carlos Velasco <carlos.velasco@lequa.com>
2018-05-21 16:11:31 +0200

    remove birdwave

--- prod.js
+++ prod.js
@@ -1,78 +1,120 @@
-const ImageminPlugin = require('imagemin-webpack-plugin')
-  .default;
-const HtmlMinifierPlugin = require('html-minifier-webpack-plugin');
-const ClosureCompilerPlugin = require('webpack-closure-compiler');
-const OfflinePlugin = require('offline-plugin');
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const HtmlMinifierPlugin = require("html-minifier-webpack-plugin");
+// const ClosureCompilerPlugin = require('webpack-closure-compiler');
+const OfflinePlugin = require("offline-plugin");
 
-module.exports = function (env) {
+module.exports = function(env) {
   return {
-    entry: './entry.js',
+    entry: "./entry.js",
     output: {
       path: __dirname,
-      filename: 'bundle.js',
+      filename: "bundle.js"
     },
-    devtool: 'cheap-module-source-map',
+    devtool: "cheap-module-source-map",
     module: {
-      rules: [{
-        test: /indexB.html$/,
-        loaders: ['file-loader?name=index.[ext]', 'extract-loader', 'html-loader'],
-      }, {
-        test: /embedEnB.html$/,
-        loaders: ['file-loader?name=embedEn.[ext]', 'extract-loader', 'html-loader'],
-      }, {
-        test: /embedSvB.html$/,
-        loaders: ['file-loader?name=embedSv.[ext]', 'extract-loader', 'html-loader'],
-      }, {
-        test: /iframeEnB.html$/,
-        loaders: ['file-loader?name=iframeEn.[ext]', 'extract-loader', 'html-loader'],
-      }, {
-        test: /iframeSvB.html$/,
-        loaders: ['file-loader?name=iframeSv.[ext]', 'extract-loader', 'html-loader'],
-      }, {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      }, {
-        test: /\.(png|gif|jpg|webp)$/,
-        use: ['file-loader?name=[path][name].[ext]'],
-      }, {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=[path][name].[ext]',
-      }, {
-        test: /\.svg$/,
-        use: [{
-          loader: 'file-loader?name=[path][name].[ext]',
-        }],
-      }, {
-        test: /\.js$/,
-        exclude: [/node_modules/],
-        use: [{
-          loader: 'babel-loader',
-          options: { presets: ['es2015'] }
-        }],
-      }],
+      rules: [
+        {
+          test: /indexB.html$/,
+          loaders: [
+            "file-loader?name=index.[ext]",
+            "extract-loader",
+            "html-loader"
+          ]
+        },
+        {
+          test: /embedEnB.html$/,
+          loaders: [
+            "file-loader?name=embedEn.[ext]",
+            "extract-loader",
+            "html-loader"
+          ]
+        },
+        {
+          test: /embedSvB.html$/,
+          loaders: [
+            "file-loader?name=embedSv.[ext]",
+            "extract-loader",
+            "html-loader"
+          ]
+        },
+        {
+          test: /iframeEnB.html$/,
+          loaders: [
+            "file-loader?name=iframeEn.[ext]",
+            "extract-loader",
+            "html-loader"
+          ]
+        },
+        {
+          test: /iframeSvB.html$/,
+          loaders: [
+            "file-loader?name=iframeSv.[ext]",
+            "extract-loader",
+            "html-loader"
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|gif|jpg|webp)$/,
+          use: ["file-loader?name=[path][name].[ext]"]
+        },
+        {
+          test: /\.(eot|ttf|woff|woff2)$/,
+          loader: "file-loader?name=[path][name].[ext]"
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "file-loader?name=[path][name].[ext]"
+            }
+          ]
+        },
+        {
+          test: /\.js$/,
+          exclude: [/node_modules/],
+          use: [
+            {
+              loader: "babel-loader",
+              options: { presets: ["es2015"] }
+            }
+          ]
+        }
+      ]
     },
     plugins: [
       new ImageminPlugin({
         pngquant: {
-          quality: '95-100',
-        },
+          quality: "95-100"
+        }
       }),
       // ... other plugins
       new HtmlMinifierPlugin({}),
-      new ClosureCompilerPlugin({
-        compiler: {
-          language_in: 'ECMASCRIPT6',
-          language_out: 'ECMASCRIPT5',
-          compilation_level: 'ADVANCED',
-        },
-        concurrency: 8,
-      }),
+      // new ClosureCompilerPlugin({
+      //   compiler: {
+      //     language_in: 'ECMASCRIPT6',
+      //     language_out: 'ECMASCRIPT5',
+      //     compilation_level: 'ADVANCED',
+      //   },
+      //   concurrency: 8,
+      // }),
       new OfflinePlugin({
-        externals: ['./android-chrome-192x192.png', './android-chrome-512x512.png', './favicon-32x32.png', './favicon-16x16.png', './js/materialize.min.js', './js/jquery-3.2.1.min.js', './manifest.json'],
-        caches: 'all',
-        responseStrategy: 'network-first',
-        updateStrategy: 'all'
-      }),
-    ],
+        externals: [
+          "./android-chrome-192x192.png",
+          "./android-chrome-512x512.png",
+          "./favicon-32x32.png",
+          "./favicon-16x16.png",
+          "./js/materialize.min.js",
+          "./js/jquery-3.2.1.min.js",
+          "./manifest.json"
+        ],
+        caches: "all",
+        responseStrategy: "network-first",
+        updateStrategy: "all"
+      })
+    ]
   };
 };
